e17a75e7fe4401930ec63af1afc5344614f4d433
Milana Jegorova <Milana.Jegorova@ivkhk.ee>
2022-09-21 00:03:39 +0300

    выполнила первое задание

---
+++ src/sptv21task1milanajegorova/SPTV21task1MilanaJegorova.java
@@ -0,0 +1,15 @@
+
+package sptv21task1milanajegorova;
+
+
+public class SPTV21task1MilanaJegorova {
+
+
+    public static void main(String[] args) {
+       double TC = 26.6875894;
+       double TF = TC/(5d/9d)+32;;
+       System.out.println("Дана температура: " + TC + "°C");
+       System.out.println("В Фаренгейтах это: " + TF + "°F");
+    }
+    
+}
